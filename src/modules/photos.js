b985693717571ed2b4195672d8dfebb4bbf6be40
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2018-05-10 13:05:25 -0300

    simplify reducer functions

--- src/modules/photos.js
+++ src/modules/photos.js
@@ -1,10 +1,10 @@
 export const REQUEST_PHOTOS = 'photos/REQUEST_PHOTOS';
 export const RECEIVE_PHOTOS = 'photos/RECEIVE_PHOTOS';
-const FETCH_PHOTOS_URL      = "https://demo3018944.mockable.io/photos";
+const FETCH_PHOTOS_URL = 'https://demo3018944.mockable.io/photos';
 
 const initialState = {
   photos: [],
-  isFetching: false,
+  isFetching: false
 };
 
 export default (state = initialState, action) => {
@@ -12,7 +12,7 @@ export default (state = initialState, action) => {
     case REQUEST_PHOTOS:
       return {
         ...state,
-        isFetching: true,
+        isFetching: true
       };
 
     case RECEIVE_PHOTOS:
@@ -20,19 +20,12 @@ export default (state = initialState, action) => {
         ...state,
         photos: action.photos,
         isFetching: false
-      }
+      };
 
     default:
       return state;
   }
 };
-
-const receivePhotos = (json) => {
-  return {
-    type: RECEIVE_PHOTOS,
-    photos: json.results
-  }
-}
 
 const fetchPhotos = () => {
   return dispatch => {
@@ -41,28 +34,32 @@ const fetchPhotos = () => {
     });
 
     return fetch(FETCH_PHOTOS_URL)
-    .then(response => response.json())
-    .then(json => dispatch(receivePhotos(json)))
-  }
-}
+      .then(response => response.json())
+      .then(json =>
+        dispatch({
+          type: RECEIVE_PHOTOS,
+          photos: json.results
+        })
+      );
+  };
+};
 
 export const fetchPhotosIfNeeded = () => {
-
   return (dispatch, getState) => {
     if (shouldFetchPhotos(getState())) {
       // Dispatch a thunk from thunk!
-      return dispatch(fetchPhotos())
+      return dispatch(fetchPhotos());
     } else {
       // Let the calling code know there's nothing to wait for.
-      return Promise.resolve()
+      return Promise.resolve();
     }
-  }
-}
+  };
+};
 
 function shouldFetchPhotos(state) {
   if (!state.photos.photos.length) {
-    return true
+    return true;
   } else if (state.isFetching) {
-    return false
+    return false;
   }
 }
